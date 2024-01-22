fda9e9270a42a95bc030e25c9d92311bbfe84388
Dev-Wellington <joycewellingtonaprigio2@gmail.com>
2024-01-22 17:24:49 -0300

    ref e reactive

--- janeiro2024/src/components/Usuario.vue
+++ janeiro2024/src/components/Usuario.vue
@@ -1,17 +1,43 @@
 <script setup>
-import { ref } from "vue";
+import { reactive } from "vue";
+
+
+/*
 const nome = ref("Wellington dos santos silva");
+const nascimento = ref(0);
+const idade = ref(19);
+//para acessar o valor de uma variavel reativa, basta usar o .value
+console.log(idade.value);
+//para alterar o valor de uma variavel reativa, basta usar o .value
+*/
+
+//usando objeto reativo
+const pessoa = reactive({
+  nome: "",
+  dataNascimento: 0,
+  idade: 0
+  
+});
+//para alterar basta apenas usar pessoa.nome, ou pessoa.dataNascimento
+
 </script>
 
 <template>
   <div>
     <form class="formulario">
       <label for="nome">Nome</label><br />
-      <input type="text" id="nome" name="nome" v-model="nome" /><br />
+      <input type="text" id="nome" name="nome" v-model="pessoa.nome" /><br />
+      <label for="dataNascimento">Nascimento</label><br />
+      <input
+        type="number"
+        id="dataNascimento"
+        name="dataNascimento"
+        v-model="pessoa.dataNascimento"
+      /><br />
     </form>
   </div>
 
-  <p style="text-align: center;">{{ nome }}</p>
+  <p style="text-align: center">{{ pessoa.nome }} nasceu em {{ pessoa.dataNascimento }}</p>
 </template>
 
 <style scoped>
@@ -22,5 +48,4 @@ const nome = ref("Wellington dos santos silva");
   width: 200px;
   background-color: blue;
 }
-
 </style>
